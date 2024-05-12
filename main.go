4425a75abcc248d71e08a0056ebde621cdb03a13
clay-ih <160769731+claym-ih@users.noreply.github.com>
2024-05-12 12:53:45 -0400

    added samples

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"embed"
-	"embedded-js-poc/memstore"
-	"embedded-js-poc/models"
 	"io/fs"
 
-	//"embedded-nextjs-poc/models"
-	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -27,27 +23,8 @@ func main() {
 	// Take incoming requests and dispatch them to the matching handlers
 	mux := http.NewServeMux()
 
-	h := UserHandler{}
-	h.store = *memstore.NewUserMemStore()
-	h.store.Add(models.User{
-		Name:  "clay",
-		Email: "clay@ih",
-	})
-	h.store.Add(models.User{
-		Name:  "Sheila",
-		Email: "sheila@ih",
-	})
-	h.store.Add(models.User{
-		Name:  "Marcelo",
-		Email: "marcelo@ih",
-	})
 	log.Println("Configuring...")
 	mux.Handle("/", http.FileServer(http.FS(distFS)))
-	mux.HandleFunc("GET /api/user/{id}", h.GetUser)
-	mux.HandleFunc("GET /api/user", h.GetUsers)
-	mux.HandleFunc("POST /api/user", h.AddUser)
-	mux.HandleFunc("PUT /api/user/{id}", h.SaveUser)
-	mux.HandleFunc("DELETE /api/user/{id}", h.DeleteUser)
 
 	// Run the server
 	server := &http.Server{
@@ -57,85 +34,4 @@ func main() {
 	log.Println("Listening...")
 	server.ListenAndServe() // Run the http server
 
-}
-
-func (h *UserHandler) errorResponse(w http.ResponseWriter, statusCode int, errorString string) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	encodingError := json.NewEncoder(w).Encode(UserError{
-		StatusCode: statusCode,
-		Error:      errorString,
-	})
-	if encodingError != nil {
-		http.Error(w, encodingError.Error(), http.StatusInternalServerError)
-	}
-}
-
-type UserError struct {
-	StatusCode int    `json:"status_code"`
-	Error      string `json:"error"`
-}
-
-func (h *UserHandler) Home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is my home page"))
-}
-
-func (h *UserHandler) GetUsers(w http.ResponseWriter, r *http.Request) {
-	users, err := h.store.List()
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(users)
-	if err != nil {
-		h.errorResponse(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-}
-
-func (h *UserHandler) GetUser(w http.ResponseWriter, r *http.Request) {
-	id := r.PathValue("id")
-	user, err := h.store.Get(id)
-	if err != nil {
-		h.errorResponse(w, http.StatusNotFound, err.Error())
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(user)
-
-}
-
-func (h *UserHandler) AddUser(w http.ResponseWriter, r *http.Request) {
-	var user models.User
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		h.errorResponse(w, http.StatusBadRequest, err.Error())
-	}
-	newUser := h.store.Add(user)
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(newUser)
-}
-
-func (h *UserHandler) SaveUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	id := r.PathValue("id")
-	var user models.User
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		h.errorResponse(w, http.StatusBadRequest, err.Error())
-	}
-	user, err = h.store.Update(id, user)
-	if err != nil {
-		h.errorResponse(w, http.StatusBadRequest, err.Error())
-	}
-	err = json.NewEncoder(w).Encode(user)
-}
-
-func (h *UserHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
-	id := r.PathValue("id")
-	err := h.store.Remove(id)
-	if err != nil {
-		h.errorResponse(w, http.StatusNotFound, err.Error())
-	}
-}
-
-type UserHandler struct {
-	store memstore.UserMemStore
 }
